0f9c4c88237d2f1bb3151cc19fb800c99c195ee3
rrcchirinosc <rrchirinosc@gmail.com>
2021-11-25 01:44:49 +0100

    - Added cart button component to link to Cart component and show the number of items added to the cart

---
+++ ClientApp/src/components/CartButtonComponent.js
@@ -0,0 +1,21 @@
+﻿import React, { Component } from 'react';
+
+export class CartButton extends Component {
+    static displayName = CartButton.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { currentCount: 0 };
+    }
+
+    render() {
+        return (
+            <a href="/cart">
+                <div className="d-flex cart-button">                
+                    <h6 className="p-2">Cart</h6>
+                    <h6 className="cart-items-count p-2">4</h6>                
+                </div>
+            </a>
+        );
+    }
+}

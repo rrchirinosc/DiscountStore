81d64eef105d21386f8fde6e80edf45fb86af228
rrcchirinosc <rrchirinosc@gmail.com>
2021-12-01 02:08:48 +0100

    - Changed to functional component - Updated to show all available items - Keeps track of selected items

--- ClientApp/src/components/HomeComponent.js
+++ ClientApp/src/components/HomeComponent.js
@@ -1,67 +1,68 @@
-﻿import React, { Component } from 'react';
-import { Card, CardBody, CardTitle, CardText, CardFooter } from 'reactstrap';
+﻿import React from 'react';
+import { Card, CardBody, CardTitle, CardText, CardFooter, Button } from 'reactstrap';
 
+function DisplayDiscount({ item }) {
 
-export class Home extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = { items: [], loading: true };
-
-        //this.updateTable = this.updateTable.bind(this);
+    if (item.discountPrice !== 0) {
+        return (
+            <CardText className="discount-text">Get {item.discountCount} for {item.discountPrice}&#8364;
+            </CardText>
+        );
     }
+    return null;
+}
 
-    /* Load members table on start */
-    componentDidMount() {
-        this.fetchItems();
+function addItemToCart(items, item, onCartChanged) {
+
+    item.count++;
+    onCartChanged(items);
+}
+
+/* Render sale items */
+function RenderItemsTable({ items, onCartChanged }) {
+
+   return (
+        <div className="col-lg-8">
+           {items.map(item =>
+               <Card key={item.id} className="my-4">
+                    <CardBody>
+                        <CardTitle className="d-flex justify-content-between">
+                            <div><span className="item-data">{item.sku}</span></div>
+                           <div>Price: <span className="item-data">{item.price}&#8364;</span></div>
+                        </CardTitle>
+                        <DisplayDiscount item={item}/>
+                        <CardText>{item.description}</CardText>
+                    </CardBody>
+                    <CardFooter className="card-footer">
+                        <Button className="btn btn-warning float-right" onClick={() => addItemToCart(items, item, onCartChanged)}>Add to Cart</Button>
+                    </CardFooter>
+                </Card>
+            )}
+        </div>
+    );
+}
+
+
+function Home(props) {
+
+    const { items, loading, onCartChanged } = props;
+
+    if (loading) {
+        return (
+            <div>Loading...</div>
+        );
     }
-
-    static displayName = Home.name;
-
-    /* Render members table */
-    static renderItemsTable(items) {
-            return (
-                <div className="col-lg-8">
-                    {items.map(item =>     
-                        <Card className="my-4">
-                            <CardBody>
-                                <CardTitle className="d-flex justify-content-between">
-                                    <div><span className="item-data">{item.sku}</span></div>
-                                    <div>Price: <span className="item-data">{item.price}€</span></div>
-                                </CardTitle>
-                                {item.discountPrice ? <CardText className="discount-text">{item.discountCount} for {item.discountPrice}€ </CardText> : <CardText className="discount-text-clear">"-"</CardText>}
-                                <CardText>{item.description}</CardText>                                
-                            </CardBody>
-                            <CardFooter className="card-footer">
-                                <a href="#" className="btn btn-warning float-right">Add to Cart</a>
-                            </CardFooter>
-                        </Card>
-                    )}
-                </div>
-            );
-    }
-
-    /* Retrieve all available items */
-    async fetchItems() {
-        const response = await fetch('/api/items');
-        const data = await response.json();
-        //console.log(await response.text());
-        this.setState({ items: data, loading: false });
-    }
-
-    render() {
-
-        let items = this.state.loading
-            ? <p><em>Loading...</em></p>
-            : Home.renderItemsTable(this.state.items);
-
+    else {    
         return (
             <React.Fragment>
                 <div className="col-lg-12">
-                    <h5>Sale Items</h5>
-                    {items}
+                    <h3 className="page-header">Sale Items</h3>                
+                    <RenderItemsTable items={items} onCartChanged={onCartChanged}/>
                 </div>
             </React.Fragment>
         );
-    }
+     }
 }
+
+export default Home;
+

3aac58bc8935720bc9bd415faa76d130e41175c5
rrcchirinosc <rrchirinosc@gmail.com>
2021-12-01 02:02:54 +0100

    - Changed to functional component - Button shows the number of items the user has selected

--- ClientApp/src/components/CartButtonComponent.js
+++ ClientApp/src/components/CartButtonComponent.js
@@ -1,21 +1,19 @@
-﻿import React, { Component } from 'react';
+﻿import React from 'react';
+import { Link } from 'react-router-dom';
 
-export class CartButton extends Component {
-    static displayName = CartButton.name;
 
-    constructor(props) {
-        super(props);
-        this.state = { currentCount: 0 };
-    }
+function CartButton(props) {
+    const { cartItemsCount } = props;
 
-    render() {
-        return (
-            <a href="/cart">
-                <div className="d-flex cart-button">                
-                    <h6 className="p-2">Cart</h6>
-                    <h6 className="cart-items-count p-2">4</h6>                
-                </div>
-            </a>
-        );
-    }
+    return (
+        <Link to="/cart">
+            <div className="d-flex cart-button">
+                <h4 className="p-2">Cart</h4>
+                <h4 className="cart-items-count p-2">{cartItemsCount}</h4>
+            </div>
+        </Link>
+    );
 }
+
+export default CartButton;
+

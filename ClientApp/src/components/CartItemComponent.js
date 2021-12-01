de20bc31ac4af9d80d9b46847d254c909fb2b29a
rrcchirinosc <rrchirinosc@gmail.com>
2021-12-01 02:06:58 +0100

    - Changed to functional component - Not used yet but will hold the item display area (card)

---
+++ ClientApp/src/components/CartItemComponent.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import { Card, CardBody, CardTitle, CardText, CardFooter, Button } from 'reactstrap';
+
+function CartItem(props) {
+
+    const { items } = props;
+
+    return (
+        <div className="col-lg-8">
+            {items.map(item =>
+                <Card key={item.id}>
+                    <CardBody>
+                        <CardTitle className="d-flex justify-content-between">
+                            <div><span className="item-data">{item.sku}</span></div>
+                            <div>Price: <span className="item-data">{item.price}&#8364;</span></div>
+                        </CardTitle>
+                        <DisplayDiscount item={item} />
+                        <CardText>{item.description}</CardText>
+                    </CardBody>                    
+                </Card>
+            )}
+        </div>
+        );
+}

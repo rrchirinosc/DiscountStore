2802c82465b0bfb285dfd44fcfdcf58efabde915
rrcchirinosc <rrchirinosc@gmail.com>
2021-12-01 02:09:07 +0100

    - Changed to functional component

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,41 +1,27 @@
-import React, { Component } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavItem } from 'reactstrap';
+import React from 'react';
+import { Container, Navbar, NavbarBrand, NavItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import './NavMenu.css';
-import { CartButton } from './CartButtonComponent';
+import CartButton from './CartButtonComponent';
+import '../shared/NavMenu.css';
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+function NavMenu(props) {
+    const { cartItemsCount } = props;
 
-  constructor (props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  render () {
     return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/"><h2>DiscountStore</h2></NavbarBrand>
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                    <CartButton/>
-                </NavItem>
-              </ul>
-          </Container>
-        </Navbar>
-      </header>
+        <header>
+            <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3 navbar-bk" light>
+                <Container>
+                    <NavbarBrand tag={Link} to="/"><h2>DiscountStore</h2></NavbarBrand>
+                    <ul className="navbar-nav flex-grow">
+                        <NavItem>
+                            <CartButton cartItemsCount={cartItemsCount}/>
+                        </NavItem>
+                    </ul>
+                </Container>
+            </Navbar>
+        </header>
     );
-  }
 }
+
+export default NavMenu;
+

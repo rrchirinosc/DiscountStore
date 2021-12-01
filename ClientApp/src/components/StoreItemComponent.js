1092e9dca7190fe4652c66e8822acb6a426d342e
rrcchirinosc <rrchirinosc@gmail.com>
2021-12-01 12:44:41 +0100

    - Changed CartItemComponent to StoreItemComponent to be more clear when using it in the Home component. This component is used on both Home and Cart components

---
+++ ClientApp/src/components/StoreItemComponent.js
@@ -0,0 +1,33 @@
+﻿import React from 'react';
+import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
+
+function DisplayDiscount({ item }) {
+
+    if (item.discountPrice !== 0) {
+        return (
+            <CardText className="discount-text">Get {item.discountCount} for {item.discountPrice}&#8364;
+            </CardText>
+        );
+    }
+    return null;
+}
+
+function StoreItem(props) {
+
+    const { item } = props;
+
+    return (
+           <Card>
+                <CardBody>
+                    <CardTitle className="d-flex justify-content-between">
+                        <div><span className="item-data">{item.sku}</span></div>
+                        <div>Price: <span className="item-data">{item.price}&#8364;</span></div>
+                    </CardTitle>
+                    <DisplayDiscount item={item} />
+                    <CardText>{item.description}</CardText>
+                </CardBody>                    
+            </Card>
+        );
+}
+
+export default StoreItem;
